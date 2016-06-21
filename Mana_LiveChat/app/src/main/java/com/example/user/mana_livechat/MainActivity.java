a04c8e747549933054249777f8c4587fd28be4af
AgusDhito <aardhito212@gmail.com>
2016-06-21 10:55:26 +0700

    part 1
    part 1

--- Mana_LiveChat/app/src/main/java/com/example/user/mana_livechat/MainActivity.java
+++ Mana_LiveChat/app/src/main/java/com/example/user/mana_livechat/MainActivity.java
@@ -135,11 +135,11 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            if (position == 1) {
+                return new Chats();
+            } else
+                return PlaceholderFragment.newInstance(position + 1);
         }
-
         @Override
         public int getCount() {
             // Show 3 total pages.
