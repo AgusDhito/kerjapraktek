dd063428d308dd60fb665b4f226f0847cf5743d1
AgusDhito <aardhito212@gmail.com>
2016-07-19 16:03:16 +0700

    udah bisa tabbed
    udah bisa tabbed

---
+++ Mana_LiveChatv2/Mana_LiveChatv2/app/src/main/java/com/example/user/mana_livechatv2/TestActivity.java
@@ -0,0 +1,14 @@
+package com.example.user.mana_livechatv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 19/07/2016.
+ */
+public class TestActivity extends Activity{
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
