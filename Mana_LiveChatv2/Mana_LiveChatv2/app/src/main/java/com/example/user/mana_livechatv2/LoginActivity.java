af956a587c87d105f5b87aa7a2e69d5b9796d7d2
karuniayusuf <karuniayusuf17@gmail.com>
2016-07-21 15:42:17 +0700

    ketinggalan

--- Mana_LiveChatv2/Mana_LiveChatv2/app/src/main/java/com/example/user/mana_livechatv2/LoginActivity.java
+++ Mana_LiveChatv2/Mana_LiveChatv2/app/src/main/java/com/example/user/mana_livechatv2/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.mana_livechatv2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,7 +19,9 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -39,10 +43,16 @@ import com.example.user.mana_livechatv2.app.MyApplication;
 import com.example.user.mana_livechatv2.model.User;
 
 public class LoginActivity extends AppCompatActivity {
+
     private String TAG = LoginActivity.class.getSimpleName();
-    private EditText inputName, inputEmail;
-    private TextInputLayout inputLayoutName, inputLayoutEmail;
-    private Button btnEnter;
+    private EditText inputName, inputEmail, inputPassword;
+//    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPassword;
+    private CheckBox saveLoginCheckBox;
+    private Button btnLogin, btnToRegister;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
          * redirect him to main activity
          * */
         if (MyApplication.getInstance().getPrefManager().getUser() != null) {
-            startActivity(new Intent(this, MainActivity.class));
+            startActivity(new Intent(this, TabbedActivity.class));
             finish();
         }
 
@@ -60,156 +70,202 @@ public class LoginActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
-        inputLayoutEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
+//        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+//        inputLayoutEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
+//        inputLayoutPassword = (TextInputLayout) findViewById(R.id.input_layout_password);
         inputName = (EditText) findViewById(R.id.input_name);
         inputEmail = (EditText) findViewById(R.id.input_email);
-        btnEnter = (Button) findViewById(R.id.btn_enter);
+//        inputPassword = (EditText) findViewById(R.id.input_password);
 
-        inputName.addTextChangedListener(new MyTextWatcher(inputName));
-        inputEmail.addTextChangedListener(new MyTextWatcher(inputEmail));
+        saveLoginCheckBox = (CheckBox)findViewById(R.id.saveLoginCheckBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnToRegister = (Button) findViewById(R.id.btn_to_register);
 
-        btnEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
+//        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+//        inputEmail.addTextChangedListener(new MyTextWatcher(inputEmail));
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            inputName.setText(loginPreferences.getString("name", ""));
+            inputEmail.setText(loginPreferences.getString("email", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View view) {
                 login();
+            }
+        });
+
+        btnToRegister.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+                finish();
             }
         });
     }
 
     private void login() {
-        if (!validateName()) {
-            return;
+//        if (!validateName()) {
+//            return;
+//        }
+//
+//        if (!validateEmail()) {
+//            return;
+//        }
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(inputName.getWindowToken(), 0);
+
+        final String name = inputName.getText().toString().trim();
+        final String email = inputEmail.getText().toString().trim();
+
+        if (saveLoginCheckBox.isChecked()) {
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            loginPrefsEditor.putString("name", name);
+            loginPrefsEditor.putString("email", email);
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
         }
 
-        if (!validateEmail()) {
-            return;
-        }
+        // Check for empty data in the form
+        if (!name.isEmpty() && !email.isEmpty()) {
+            // login user
+            StringRequest strReq = new StringRequest(Request.Method.POST,
+                    EndPoints.LOGIN, new Response.Listener<String>() {
 
-        final String name = inputName.getText().toString();
-        final String email = inputEmail.getText().toString();
+                @Override
+                public void onResponse(String response) {
+                    Log.e(TAG, "response: " + response);
 
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                EndPoints.LOGIN, new Response.Listener<String>() {
+                    try {
+                        JSONObject obj = new JSONObject(response);
 
-            @Override
-            public void onResponse(String response) {
-                Log.e(TAG, "response: " + response);
+                        // check for error flag
+                        if (obj.getBoolean("error") == false) {
+                            // user successfully logged in
 
-                try {
-                    JSONObject obj = new JSONObject(response);
+                            JSONObject userObj = obj.getJSONObject("user");
+                            User user = new User(userObj.getString("user_id"),
+                                    userObj.getString("name"),
+                                    userObj.getString("email"));
 
-                    // check for error flag
-                    if (obj.getBoolean("error") == false) {
-                        // user successfully logged in
+                            // storing user in shared preferences
+                            MyApplication.getInstance().getPrefManager().storeUser(user);
 
-                        JSONObject userObj = obj.getJSONObject("user");
-                        User user = new User(userObj.getString("user_id"),
-                                userObj.getString("name"),
-                                userObj.getString("email"));
+                            // start main activity
+                            startActivity(new Intent(getApplicationContext(), TabbedActivity.class));
+                            finish();
 
-                        // storing user in shared preferences
-                        MyApplication.getInstance().getPrefManager().storeUser(user);
+                        } else {
+                            // login error - simply toast the message
+                            Toast.makeText(getApplicationContext(), "" + obj.getJSONObject("error").getString("message"), Toast.LENGTH_LONG).show();
+                        }
 
-                        // start main activity
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        finish();
-
-                    } else {
-                        // login error - simply toast the message
-                        Toast.makeText(getApplicationContext(), "" + obj.getJSONObject("error").getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Log.e(TAG, "json parsing error: " + e.getMessage());
+                        Toast.makeText(getApplicationContext(), "Json parse error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-
-                } catch (JSONException e) {
-                    Log.e(TAG, "json parsing error: " + e.getMessage());
-                    Toast.makeText(getApplicationContext(), "Json parse error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
-            }
-        }, new Response.ErrorListener() {
+            }, new Response.ErrorListener() {
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                NetworkResponse networkResponse = error.networkResponse;
-                Log.e(TAG, "Volley error: " + error.getMessage() + ", code: " + networkResponse);
-                Toast.makeText(getApplicationContext(), "Volley error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }) {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    NetworkResponse networkResponse = error.networkResponse;
+                    Log.e(TAG, "Volley error: " + error.getMessage() + ", code: " + networkResponse);
+                    Toast.makeText(getApplicationContext(), "Volley error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }) {
 
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("name", name);
-                params.put("email", email);
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("name", name);
+                    params.put("email", email);
 
-                Log.e(TAG, "params: " + params.toString());
-                return params;
-            }
-        };
+                    Log.e(TAG, "params: " + params.toString());
+                    return params;
+                }
+            };
 
-        //Adding request to request queue
-        MyApplication.getInstance().addToRequestQueue(strReq);
-    }
-
-    private void requestFocus(View view) {
-        if (view.requestFocus()) {
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-        }
-    }
-
-    // Validating name
-    private boolean validateName() {
-        if (inputName.getText().toString().trim().isEmpty()) {
-            inputLayoutName.setError(getString(R.string.err_msg_name));
-            requestFocus(inputName);
-            return false;
+            //Adding request to request queue
+            MyApplication.getInstance().addToRequestQueue(strReq);
         } else {
-            inputLayoutName.setErrorEnabled(false);
+            // Prompt user to enter credentials
+            Toast.makeText(getApplicationContext(),
+                    "Data belum lengkap!", Toast.LENGTH_LONG).show();
         }
 
-        return true;
     }
 
-    // Validating email
-    private boolean validateEmail() {
-        String email = inputEmail.getText().toString().trim();
+//    private void requestFocus(View view) {
+//        if (view.requestFocus()) {
+//            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+//        }
+//    }
 
-        if (email.isEmpty() || !isValidEmail(email)) {
-            inputLayoutEmail.setError(getString(R.string.err_msg_email));
-            requestFocus(inputEmail);
-            return false;
-        } else {
-            inputLayoutEmail.setErrorEnabled(false);
-        }
+//    // Validating name
+//    private boolean validateName() {
+//        if (inputName.getText().toString().trim().isEmpty()) {
+//            inputLayoutName.setError(getString(R.string.err_msg_name));
+//            requestFocus(inputName);
+//            return false;
+//        } else {
+//            inputLayoutName.setErrorEnabled(false);
+//        }
+//
+//        return true;
+//    }
+//
+//    // Validating email
+//    private boolean validateEmail() {
+//        String email = inputEmail.getText().toString().trim();
+//
+//        if (email.isEmpty() || !isValidEmail(email)) {
+//            inputLayoutEmail.setError(getString(R.string.err_msg_email));
+//            requestFocus(inputEmail);
+//            return false;
+//        } else {
+//            inputLayoutEmail.setErrorEnabled(false);
+//        }
+//
+//        return true;
+//    }
 
-        return true;
-    }
+//    private static boolean isValidEmail(String email) {
+//        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+//    }
 
-    private static boolean isValidEmail(String email) {
-        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
+//    private class MyTextWatcher implements TextWatcher {
+//
+//        private View view;
+//        private MyTextWatcher(View view) {
+//            this.view = view;
+//        }
+//
+//        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//        }
+//
+//        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//        }
+//
+//        public void afterTextChanged(Editable editable) {
+//            switch (view.getId()) {
+//                case R.id.input_name:
+//                    validateName();
+//                    break;
+//                case R.id.input_email:
+//                    validateEmail();
+//                    break;
+//            }
+//        }
+//    }
 
-    private class MyTextWatcher implements TextWatcher {
-
-        private View view;
-        private MyTextWatcher(View view) {
-            this.view = view;
-        }
-
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void afterTextChanged(Editable editable) {
-            switch (view.getId()) {
-                case R.id.input_name:
-                    validateName();
-                    break;
-                case R.id.input_email:
-                    validateEmail();
-                    break;
-            }
-        }
-    }
 }
